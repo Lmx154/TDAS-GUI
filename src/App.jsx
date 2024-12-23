d46fb572dc1690e7d420454921c55053c1c55fde
Luis <luis_mtz907@live.com>
2024-12-23 16:12:50 -0600

    removed from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,19 @@
 //CAN THIS BE DONE IN A SIMPLER WAY? (EG. USING A LIBRARY, A FRAMEWORK, ETC)
 //DOES THIS HAVE A LOT OF STYLING? (EG. CSS, JAVASCRIPT, ETC)
 //IF YOU ANSWERED YES TO ANY OF THE ABOVE, PLEASE MOVE TO THE APPROPRIATE FILE, OR CREATE A NEW ONE
-//THIS FILE SHOULD ONLY CONTAIN THE FRONTEND LOGIC, EG. REACT COMPONENTS, STATE MANAGEMENT, ETC
+//THIS FILE SHOULD ONLY CONTAIN THE FRONTEND LOGIC, EG. REACT COMPONENTS, STATE MANAGEMENT, ROUTING, ETC
 
-import { Route, Link, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import TestPage from "./pages/test.jsx";
+// ...import other pages as needed...
 
 function App() {
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-xl font-bold text-red-500">Welcome</h1>
-      <Link to="/test" className="text-blue-500 hover:underline">
-        Go to Test Page
-      </Link>
-      <Routes>
-        <Route path="/" element={<div />} />
-        <Route path="/test" element={<TestPage />} />
-      </Routes>
-    </main>
+    <Routes>
+      <Route path="/" element={<Navigate to="/test" />} />
+      <Route path="/test" element={<TestPage />} />
+      {/* Add more routes to other pages here */}
+    </Routes>
   );
 }
 
